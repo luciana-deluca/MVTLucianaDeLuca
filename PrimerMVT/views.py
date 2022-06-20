522c2e96f0ecc0c7bf2d5d4ffad52f1d9a65bf42
luciana-deluca <lucianadelucaaa@gmail.com>
2022-06-20 15:00:18 -0300

    Se levanto vista con el template

--- PrimerMVT/views.py
+++ PrimerMVT/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from PrimerMVT import Familia
-
+from django.template import loader
+from django.http import HttpResponse
 
 def familia(self):
     familia= Familia(nombre="Vanesa", fechadenac="1979-01-28", edad="43")
     familia.save()
+    temp = loader.get_template("templatemvt.html")
+    documento = temp.render(familia)
+    return HttpResponse(documento)
     
